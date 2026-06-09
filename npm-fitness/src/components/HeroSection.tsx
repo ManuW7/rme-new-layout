f9501a83692851f46c34ec246f878c01c1471a51
mnl <mamontovgleb7@gmail.com>
2026-06-09 18:36:50 +0300

    create hero screen

---
+++ npm-fitness/src/components/HeroSection.tsx
@@ -0,0 +1,27 @@
+import "./HeroSection.css";
+import manHero from "../assets/manHero.png";
+import phoneHero from "../assets/phone_hero.png";
+
+function HeroSection() {
+  return (
+    <section className="heroSection">
+      <div className="heroSectionContent">
+        <h1>
+          <span>ПЕРСОНАЛЬНЫЙ ИИ-ТРЕНЕР</span> У ТЕБЯ В ТЕЛЕФОНЕ
+        </h1>
+        <p>
+          Занимайся фитнесом и&nbsp;йогой из&nbsp;любой точки мира
+          и&nbsp;в&nbsp;любое время
+        </p>
+        <a className="tryButton" href="">
+          Попробовать бесплатно
+        </a>
+      </div>
+      <div className="greenCircleBackground"></div>
+      <img className="manPicture" src={manHero} alt="" />
+      <img className="phonePicture" src={phoneHero} alt="" />
+    </section>
+  );
+}
+
+export default HeroSection;
