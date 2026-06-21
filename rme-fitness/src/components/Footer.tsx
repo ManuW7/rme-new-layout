ae8cbbdcd16465a53c8ce872187db26f4a83e001
mnl <mamontovgleb7@gmail.com>
2026-06-21 19:05:45 +0300

    minor changes

--- rme-fitness/src/components/Footer.tsx
+++ rme-fitness/src/components/Footer.tsx
@@ -16,13 +16,21 @@ function Footer({ onOpenAppModal }: FooterProps) {
         <div className="footerColumns">
           <div className="footerColumn footerStores">
             <h4>Доступно в</h4>
-            <button type="button" aria-label="Google Play" onClick={onOpenAppModal}>
+            <button
+              type="button"
+              aria-label="Google Play"
+              onClick={onOpenAppModal}
+            >
               <img src={googlePlayIcon} alt="" />
             </button>
             <button type="button" aria-label="RuStore" onClick={onOpenAppModal}>
               <img src={ruStoreIcon} alt="" />
             </button>
-            <button type="button" aria-label="App Store" onClick={onOpenAppModal}>
+            <button
+              type="button"
+              aria-label="App Store"
+              onClick={onOpenAppModal}
+            >
               <img src={appStoreIcon} alt="" />
             </button>
           </div>
@@ -39,17 +47,27 @@ function Footer({ onOpenAppModal }: FooterProps) {
           <nav className="footerColumn" aria-label="Контакты">
             <h4>Контакты</h4>
             <a href="">info@rme-fitness.ru</a>
-            <a href="">Связаться с нами</a>
-            <a href="">Телеграм канал</a>
-            <a href="">Сайт онлайн-записи</a>
+            <a href="https://forms.yandex.ru/u/68ebf02ef47e739315df20d8">
+              Связаться с нами
+            </a>
+            <a href="t.me/RMe_channel">Телеграм канал</a>
+            <a href="https://r-me.tech/ru">Сайт головной компании</a>
           </nav>
 
           <nav className="footerColumn" aria-label="Дополнительно">
             <h4>Дополнительно</h4>
-            <a href="">Политика конфиденциальности</a>
-            <a href="">Пользовательское соглашение</a>
-            <a href="">Политика обработки персональных данных</a>
-            <a href="">Партнерство</a>
+            <a href="https://docs.google.com/document/d/1yPACyd2OwYVY1SwQvlkR1wHGJr8TPXCplxoBQtyHinY/edit?tab=t.0">
+              Политика конфиденциальности
+            </a>
+            <a href="https://docs.google.com/document/d/1dgo39mV0zOhxStnz1P_IqYOIP_5hYecQIxtONDpjt4Y/edit?tab=t.0">
+              Пользовательское соглашение
+            </a>
+            <a href="https://docs.google.com/document/d/1yPACyd2OwYVY1SwQvlkR1wHGJr8TPXCplxoBQtyHinY/edit?tab=t.0">
+              Политика обработки персональных данных
+            </a>
+            <a href="https://forms.yandex.ru/u/693b249bf47e73185bc4f0e2">
+              Партнерство
+            </a>
           </nav>
         </div>
 
